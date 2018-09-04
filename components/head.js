44d61ec7aec300c2bdf89682b7d45950807a8781
PC-20180522-03\Administrator <492020314@qq.com>
2018-09-04 18:32:30 +0800

    应seo强烈要求修改与百度统计代码一模一样

--- components/head.js
+++ components/head.js
@@ -9,9 +9,9 @@ const Head = (props) => (
     <meta charSet="UTF-8" />
     <title>{props.title || ''}</title>
     <meta name="description" content={props.description || defaultDescription} />
-    <meta name="viewport" content="initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no"/>
-    <meta name="baidu-site-verification" content="hMF9xpWYyj" /> 
-    {props.url?<link rel="canonical" href={`http://${props.url}`}/>:''}
+    <meta name="viewport" content="initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no" />
+    <meta name="baidu-site-verification" content="hMF9xpWYyj" />
+    {props.url ? <link rel="canonical" href={`http://${props.url}`} /> : ''}
     <link rel="icon" sizes="192x192" href="/static/favicon.ico" />
     <link rel="apple-touch-icon" href="/static/favicon.ico" />
     <link rel="mask-icon" href="/static/favicon.ico" color="#49B882" />
@@ -29,17 +29,17 @@ const Head = (props) => (
     <meta property="og:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" /> */}
-    <script type="text/javascript">
-       {` var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?6f3aef8faa233258d8880a4b7a489db0";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();   `}
-        </script>
+    <script>
+      {` var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?6f3aef8faa233258d8880a4b7a489db0";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();   `}
+    </script>
   </NextHead>
-  
+
 )
 
 Head.propTypes = {
